f6163263f0dc47033b81b4ffb14bbeeff8e8a7af
naga-g4 <pranodg4@gmail.com>
2022-03-14 19:26:59 -0400

    register user

---
+++ Website/register.php
@@ -0,0 +1,79 @@
+<?php
+
+session_start();
+
+#connect to db
+#require_once 'name of the database'
+
+$user_name = $_POST["name"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$conf_password = $_POST["confpassword"];
+
+#checks for any empty fields
+function empty_fields($user_name, $email, $password, $conf_password) {
+    if(empty($user_name) || ($email) || ($password) || ($conf_password)) {
+        $val;
+        $val = true;
+    }
+    else {
+        $val;
+        $val = false;
+    }
+    return $val;
+}
+
+#checks to see if username is taken 
+function invalid_username($password, $conf_password) {
+    
+}
+
+#checks if email format is correct and if email is taken
+function invalid_email($email) {
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $val;
+        $val = true;
+    }
+    else {
+        $val;
+        $val = false;
+    }
+    return $val;
+}
+
+#checks to see the passwords match
+function invalid_pass($password, $conf_password) {
+    if($password !== $conf_password) {
+    $val;
+    $val = true;
+}
+else {
+        $val;
+        $val = false;
+    }
+    return $val;
+}
+
+
+
+if (empty_fields() === true) {
+    #didnt fillout the form
+    exit();
+}
+
+if (invalid_username() === true) {
+    #username already taken
+    exit();
+}
+
+if (invalid_email() === true) {
+    #email already in use
+    exit();
+}
+
+if (invalid_pass() === true) {
+    #passwords dont match
+    exit();
+}
+
+register_user($conn, $user_name, $email, $password);
