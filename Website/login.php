a7812418115a171b1db932c377255d223aad67a9
Brett24455 <bj244@njit.edu>
2022-03-16 19:21:29 -0400

    Added a session to the web server for basic authentication, will redirect to login.html if not properly authenticated

--- Website/login.php
+++ Website/login.php
@@ -1,8 +1,14 @@
-#!/usr/bin/php
 <?php
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
+include('config.php');
+
+function sanitize($field){
+        $field = trim($field);
+        return $field;
+}
+
 
 function login($username, $password){
 
@@ -30,22 +36,26 @@ function login($username, $password){
 	echo $argv[0]." END".PHP_EOL;
 
 
-	if(!$response){
-		//$client->close();	
+	if(!$response){	
 		return false; 
 	}
-	//$client->close();
 	return true;
 }
 
-$user = $_GET["username"];
-$pass = $_GET["password"];
+$user = $_GET["username"]; $pass = $_GET["password"];
+$user = sanitize($user); $pass = sanitize($pass);
+
 
 if(login($user, $pass)){
-	echo "<br>Logged In.";
+	echo "<br>Logged In. Redirecting.";
+	$_SESSION["logged"] = true;
+	$_SESSION["user"] = $user;
+	header( "refresh: 3; url=homepage.php" );
+	exit();
 }
 else{
 	echo "<br>Failed login.";
+	header( "refresh: 3; url=login.html" );
 	exit();
 }
 ?>
